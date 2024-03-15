e1325b799ccc93dacc027b64c99417641e82cea0
Ralston Berry <b8047585@gmail.com>
2024-03-15 18:56:20 +0200

    Added refactored addNewGoals code

--- index.js
+++ index.js
@@ -19,6 +19,37 @@ document.querySelector('#submitWorkout').addEventListener('click', displayWorkou
 // NOW LET'S DEBUG TO PREVENT DUPLICATE GOALS FROM BEING SUBMITTED 🚀
 
 
+// This code allow the user to enter a goal and add it to a list.
+// If it detects a duplicate it displays a message stating that the goal already exists
+// The code also allows you to remove goals from the list by clicking on it
+const addNewGoal = () => {
+    const goalInput = document.querySelector('#goalInput').value;
+    const goalList = document.querySelector('#goalList');
+    let arrGoals = goalList.children;
+
+    // This section of the code check wheter there is any duplicates by continuously going through each item in the 
+    // list one by one untill a duplicate is found
+    let isDuplicate = false;
+    for (let i = 0; i < arrGoals.length; i++) {
+        if (goalInput === arrGoals[i].textContent) {
+            isDuplicate = true;
+            break;
+        }
+    }
+
+    // This section of the code prevents you from adding duplicates by displaying an alert message
+    // whenever the user adds a duplicate
+    if (isDuplicate) {
+        alert('Duplicates: This goal already exists.');
+    } else {
+        const newGoal = document.createElement('li');
+        newGoal.textContent = goalInput;
+        goalList.appendChild(newGoal);
+        newGoal.onclick = ()=>{
+            newGoal.remove();
+        }
+    }
+};
 
 // Add event listener to the goal submit button
 document.querySelector('#submitGoal').addEventListener('click', addNewGoal);
